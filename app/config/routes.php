d65e35efb317ea3eb65e9e3464efc590c52d34f3
Or4277 <ranaivosoaowan@gmail.com>
2026-02-16 15:20:55 +0300

    route_test

--- app/config/routes.php
+++ app/config/routes.php
@@ -26,5 +26,9 @@ $router->group('', function(Router $router) use ($app) {
 		$router->get('/users/@id:[0-9]', [ ApiExampleController::class, 'getUser' ]);
 		$router->post('/users/@id:[0-9]', [ ApiExampleController::class, 'updateUser' ]);
 	});
+
+
+
+	$route ->post('creer_don')
 	
 }, [ SecurityHeadersMiddleware::class ]);
